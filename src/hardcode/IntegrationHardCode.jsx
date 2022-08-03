0cd3aaaac8779ea769b7f1cf7b04152a46f03c1c
lti-equipamiento <kevinchenar95@gmail.com>
2022-08-03 20:36:33 -0300

    comienzo de pagina integraciones

---
+++ src/hardcode/IntegrationHardCode.jsx
@@ -0,0 +1,101 @@
+export const rows = [
+  {
+    id: 1,
+    category: "deportes",
+    difficulty: "easy",
+    quantity: 50,
+  },
+  {
+    id: 2,
+    category: "deportes",
+    difficulty: "medium",
+    quantity: 30,
+  },
+  {
+    id: 3,
+    category: "deportes",
+    difficulty: "hard",
+    quantity: 60,
+  },
+  {
+    id: 4,
+    category: "geografia",
+    difficulty: "easy",
+    quantity: 50,
+  },
+  {
+    id: 5,
+    category: "geografia",
+    difficulty: "medium",
+    quantity: 70,
+  },
+  {
+    id: 6,
+    category: "comida",
+    difficulty: "medium",
+    quantity: 70,
+  },
+  {
+    id: 7,
+    category: "comida",
+    difficulty: "hard",
+    quantity: 70,
+  },
+  {
+    id: 8,
+    category: "animales",
+    difficulty: "easy",
+    quantity: 70,
+  },
+  {
+    id: 9,
+    category: "animales",
+    difficulty: "medium",
+    quantity: 70,
+  },
+  {
+    id: 10,
+    category: "animales",
+    difficulty: "hard",
+    quantity: 70,
+  },
+  {
+    id: 11,
+    category: "trabajo",
+    difficulty: "easy",
+    quantity: 70,
+  },
+  {
+    id: 12,
+    category: "trabajo",
+    difficulty: "hard",
+    quantity: 70,
+  },
+];
+
+export const categoryRows = [
+  {
+    id: 1,
+    category: "deportes",
+  },
+  {
+    id: 2,
+    category: "comida",
+  },
+  {
+    id: 3,
+    category: "geografia",
+  },
+  {
+    id: 4,
+    category: "animales",
+  },
+  {
+    id: 5,
+    category: "trabajo",
+  },
+  {
+    id: 6,
+    category: "historia",
+  },
+];
--- src/pages/IntegrationPage.jsx
+++ src/pages/IntegrationPage.jsx
@@ -1,11 +1,128 @@
 import React, { useEffect, useContext } from "react";
 import { LayoutContextProvider } from "./../context/LayoutContext";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button, Grid, Paper, Typography } from "@mui/material";
+import { rows, categoryRows } from "../hardcode/IntegrationHardCode";
+
+const categoryColumns = [
+  { field: "id", headerName: "ID" },
+  {
+    field: "category",
+    headerName: "Categoria",
+    flex: 1,
+  },
+];
+
+const showCategoryColumns = { id: false, category: true };
+
+const columns = [
+  { field: "id", headerName: "ID" },
+  {
+    field: "category",
+    headerName: "Categoria",
+    flex: 1,
+  },
+  {
+    field: "difficulty",
+    headerName: "Dificultad",
+    flex: 1,
+  },
+  { field: "quantity", headerName: "Cantidad de preguntas", flex: 1 },
+];
+
+const showColumns = {
+  id: false,
+  category: true,
+  dificulty: true,
+  quantity: true,
+};
 
 export default function IntegrationPage() {
   const { setHeaderTitle } = useContext(LayoutContextProvider);
+  const [pageSize, setPageSize] = React.useState(10);
+
   useEffect(() => {
     setHeaderTitle("Integraciones");
   }, [setHeaderTitle]);
 
-  return <div>Integracion</div>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={10} lg={10}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            height: 360,
+          }}
+        >
+          <Grid marginLeft={2}>
+            <Typography
+              component="h2"
+              variant="h6"
+              color="primary"
+              gutterBottom
+            >
+              Categorias disponibles
+            </Typography>
+          </Grid>
+          <Grid style={{ height: 300, width: "100%" }}>
+            <DataGrid
+              rows={categoryRows}
+              columns={categoryColumns}
+              pageSize={pageSize}
+              onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+              rowsPerPageOptions={[10, 50, 100]}
+              columnVisibilityModel={showCategoryColumns}
+            />
+          </Grid>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={2} lg={2}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            height: 360,
+          }}
+        >
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth size="large">
+              Importar categoria
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth size="large">
+              Importar pregunta
+            </Button>
+          </Grid>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+          <Grid marginLeft={2}>
+            <Typography
+              component="h2"
+              variant="h6"
+              color="primary"
+              gutterBottom
+            >
+              Preguntas disponibles
+            </Typography>
+          </Grid>
+          <Grid style={{ height: 320, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={pageSize}
+              onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+              rowsPerPageOptions={[10, 50, 100]}
+              columnVisibilityModel={showColumns}
+            />
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
